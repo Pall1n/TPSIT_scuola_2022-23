9d29f024152fa2a98e92b4f75c81fdb3307f5ff6
Paolo Maria Larosa <75300377+Pall1n@users.noreply.github.com>
2023-04-24 08:57:11 +0000

    Init first exercises

---
+++ Esercizi_17_04_2023/Es_1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+    Creare un programma in c# che esegua 2 thread il primo 
+    thread scrive la lettera X e il secondo scrive la lettera 
+    Y fare scrivere il risultato in console. Commentare il risultato
+*/
+
+using System;
+using System.Threading;
+
+class Program {
+    static void Main(string[] args) {
+        Thread threadX = new Thread(new ThreadStart(ThreadX));
+        Thread threadY = new Thread(new ThreadStart(ThreadY));
+
+        threadX.Start();
+        threadY.Start();
+        
+        threadX.Join();
+        threadY.Join();
+    }
+
+    static void ThreadX() {
+        Console.WriteLine("X");
+    }
+
+    static void ThreadY() {
+        Console.WriteLine("Y");
+    }
+}
+
+/*
+    Questo codice stampa sulla console X e Y in un ordine casuale, 
+    poiché si tratta di due funzioni eseguite in modo parallelo.
+*/
---
+++ Esercizi_17_04_2023/Es_2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+    Creare un programma in c# con 2 thread il primo riempie 
+    un vettore con 100 numeri a caso il secondo riempie la 
+    seconda parte del vettore con altri numeri a caso. alla 
+    fine effettuare la somma 
+*/
+
+using System;
+using System.Threading;
+
+class Program {
+    static Random rnd = new Random();
+    static int[] vettore = new int[100];
+
+    static void Main(string[] args) {
+        Thread primaMeta = new Thread(new ThreadStart(RiempiPrimaMeta));
+        Thread secondaMeta = new Thread(new ThreadStart(RiempiSecondaMeta));
+
+        primaMeta.Start();
+        secondaMeta.Start();
+
+        primaMeta.Join();
+        secondaMeta.Join();
+
+        int somma = 0;
+        for (int i = 0; i < 100; i++) {
+            Console.WriteLine(vettore[i]);
+            somma += vettore[i];
+        }
+
+        Console.WriteLine(somma);
+    }
+
+    static void RiempiPrimaMeta() {
+        for (int i = 0; i < 50; i++) {
+            vettore[i] = rnd.Next(0, 100);
+        }
+    }
+
+    static void RiempiSecondaMeta() {
+        for (int i = 50; i < 100; i++) {
+            vettore[i] = rnd.Next(0, 100);
+        }
+    }
+}
